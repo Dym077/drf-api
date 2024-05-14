cd77fedf2ced516a0c43051f9bf40fac7a9f7ec2
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-05-14 08:52:35 +0000

    Start projecy add profile app

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-qa=m=-@m4w4voli46gq5_@#23!7n6t2xa*lbz2!uw9%!n2i_-h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-dym077-drfapi-l3150g2g81a.ws-eu111.gitpod.io']
 
 
 # Application definition
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+
+    'profiles', 
 ]
 
 MIDDLEWARE = [
