94674af003b50a404630395dde2ae42c887e24d3
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-05-17 18:16:29 +0000

    Correct typos and faulty code

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from comment.models import Comment
+from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
-    profile_id = serializers.BigAuto(source='owner.profile.id')
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
 
@@ -18,7 +18,7 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
-            'profile_image', 'created_at', 'updated_at',
+            'profile_image', 'post' 'created_at', 'updated_at',
             'content'
         ]
 
--- comments/views.py
+++ comments/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics, permissions
-from drf_api.asgi import IsOwnerOrReadOnly
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
+
 
 class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
