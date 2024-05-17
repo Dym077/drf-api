8b73fd9297399d098be5eb11910feca3d13bbcd6
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-05-17 18:23:59 +0000

    Correct typos and faulty code

--- comments/serializers.py
+++ comments/serializers.py
@@ -18,9 +18,9 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
-            'profile_image', 'post' 'created_at', 'updated_at',
+            'profile_image', 'post', 'created_at', 'updated_at',
             'content'
         ]
 
-class CommentDetailSerializer(Commentserializer):
+class CommentDetailSerializer(CommentSerializer):
     post = serializers.ReadOnlyField(source='post.id')
