fb1470b4deaa66580c626659b0a562da3537c7fc
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-05-17 14:55:32 +0000

    Add comments app

--- posts/tests.py
+++ posts/tests.py
@@ -51,3 +51,9 @@ class PostDetailViewTests(APITestCase):
         post = Post.objects.filter(pk=1).first()
         self.assertEqual(post.title, 'a new title')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_user_cant_update_another_users_post(self):
+        self.client.login(username='adam', password='pass')
+        response= self.client.put('/posts/2/', {'title': 'a new title'})
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)    
