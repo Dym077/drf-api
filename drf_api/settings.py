f9c7af6002a050eb5c49998a4dd46e45c2ead74b
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-05-27 15:25:04 +0000

    Add REST_AUTH_SERIALIZERS to settings

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -50,6 +50,10 @@ JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAIL_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
+}
+
 
 
 # Quick-start development settings - unsuitable for production
